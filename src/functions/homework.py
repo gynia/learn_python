1979d19b985b5ce1f7e1ea0d944e50f576e08537
EgAvr <auramenka87@gmail.com>
2024-05-27 03:06:41 +0300

    add func: kilometers_to_centimeters() and strip_rows()

--- src/functions/homework.py
+++ src/functions/homework.py
@@ -1,6 +1,15 @@
 # Моя домашка
 import sys
 
+def kilometers_to_centimeters(i) -> int:
+    return 1000 * i
+
+def strip_rows(rows: list) -> list:
+    ret_li = []
+    for st in rows:
+        ret_li.append(st.strip())
+    return ret_li
+
 
 if __name__ == "__main__":
     this_mod = sys.modules[__name__]
